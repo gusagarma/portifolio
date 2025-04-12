101a45310cbe343536df4d6034c9620d0762886b
gusagarma <agusmao@bosphorusgames.com>
2025-04-12 15:57:09 -0300

    Updates

--- Projects/Mask/mask.js
+++ Projects/Mask/mask.js
@@ -1,6 +1,7 @@
 // Usando CDN para importar THREE.js e GLTFLoader
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
 
 // Restante do código que você já tem com THREE.js e GLTFLoader
 
