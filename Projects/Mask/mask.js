c9eabd940138fe6fb664172a965d012a0f0e7911
gusagarma <agusmao@bosphorusgames.com>
2025-04-12 15:49:11 -0300

    Updates

--- Projects/Mask/mask.js
+++ Projects/Mask/mask.js
@@ -1,6 +1,9 @@
-// Importando via CDN ESModule
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
-import { GLTFLoader } from 'https://unpkg.com/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
+// Usando CDN para importar THREE.js e GLTFLoader
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.160.0/build/three.module.js';
+import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.160.0/examples/jsm/loaders/GLTFLoader.js';
+
+// Restante do código que você já tem com THREE.js e GLTFLoader
+
 
 // Criar a cena
 const scene = new THREE.Scene();
